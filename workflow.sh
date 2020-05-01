272a95b0678d48f7f1249e157c5e5706a4bd9fc0
davidtazy <david.allemant@gmail.com>
2020-05-01 16:06:05 +0200

    try to build from github action

--- workflow.sh
+++ workflow.sh
@@ -1,24 +1,22 @@
 #https://docs.conan.io/en/latest/developing_packages/package_dev_flow.html
 
-mkdir conan-kcov
-git clone "this" recipe
 
 
 # download sources
-conan source recipe --source-folder=tmp/source
+conan source . --source-folder=tmp/source
 
 #install dependancies
-conan install recipe --install-folder=tmp/build --profile default
+conan install . --install-folder=tmp/build --profile default
 
 
 #build
-conan build recipe --source-folder=tmp/source --build-folder=tmp/build
+conan build . --source-folder=tmp/source --build-folder=tmp/build
 
 #package
-conan package recipe --source-folder=tmp/source --build-folder=tmp/build --package-folder=tmp/package
+conan package . --source-folder=tmp/source --build-folder=tmp/build --package-folder=tmp/package
 
 #make it available locally
-conan export-pkg recipe user/channel --source-folder=tmp/source --build-folder=tmp/build --profile=default
+conan export-pkg . user/channel --source-folder=tmp/source --build-folder=tmp/build --profile=default
 
 #test it
-conan test recipe/test_package kcov/0.0.0@davidtazy/testing
+conan test ./test_package kcov/0.0.0@davidtazy/testing
